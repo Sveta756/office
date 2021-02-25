e26a057208ae42a4af841f21cf613e0bb56dc16d
Sveta <paranyushkina.sv@gmail.com>
2021-02-25 13:59:24 +0300

    js comments

--- src/js/script.js
+++ src/js/script.js
@@ -22,90 +22,75 @@ $(document).ready(function(){
 
   //ищем ul, кликаем на li, добавляем класс активности, а у остальных табов убираем. ищем родителя __view находим у него контент, удаляем класс активности и выбираем элемент с индексом как у This и добавляем класс
 
-$('ul.project__tabs').on('click', 'li:not(.project__tab_active)', function() {
-  $(this)
-    .addClass('project__tab_active').siblings().removeClass('project__tab_active')
-    .closest('div.project__view').find('div.project__content').removeClass('project__content_active').eq($(this).index()).addClass('project__content_active');
-});
-//modal 
+  $('ul.project__tabs').on('click', 'li:not(.project__tab_active)', function() {
+    $(this)
+      .addClass('project__tab_active').siblings().removeClass('project__tab_active')
+      .closest('div.project__view').find('div.project__content').removeClass('project__content_active').eq($(this).index()).addClass('project__content_active');
+  });
+  //modal 
 
-$('.button').on('click', function() {
-  $('.overlay, #modal').fadeIn();
+  $('.button').on('click', function() {
+    $('.overlay, #modal').fadeIn();
 
-});
+  });
 
-$('.modal__close').on('click', function() {
-  $('.overlay, #modal, #thanks').fadeOut();
-  $('form')[0].reset();  //сбросить значения интуп при закрытии окна
-  // $('#thanks').fadeOut();
-});
+  $('.modal__close').on('click', function() {
+    $('.overlay, #modal, #thanks').fadeOut();
+    $('form')[0].reset();  //сбросить значения интуп при закрытии окна
+    // $('#thanks').fadeOut();
+  });
 
-$('.modal__form').validate({
-  rules: {
-    name: "required",
-    phone: "required",
-    text: {
-      required: true,
-      minlength: 10
-    },
-    email: {
-      required: true,
-      email: true
-    },
-  }
-  // messages: {
-  //   name: "Пожалуйста введите свое имя",
-  //   email: {
-  //     required: "We need your email address to contact you",
-  //     email: "Your email address must be in the format of name@domain.com"
-  //   }
-  // }
-});
+  //валидация формы в модальном окне
 
-$('input[name=phone]').mask("+7 (999) 999-99-99");
+  $('.modal__form').validate({
+    rules: {
+      name: "required",
+      phone: "required",
+      text: {
+        required: true,
+        minlength: 10
+      },
+      email: {
+        required: true,
+        email: true
+      },
+    }
+  });
 
-$('form').submit(function(e) {
-  if ($('input').val() === "") {
-    return false;
-  } else {
-    e.preventDefault();
-    $.ajax({
-      type: "POST",
-      url: "mailer/smart.php",
-      data: $(this).serialize()
-    }).done(function() {
-        $(this).find("input").val("");
-        $(this).find("textarea").val("");
-        $('#modal').fadeOut();
-        $('.overlay, #thanks').fadeIn('slow');
-        $('form').trigger('reset');
-    });
-    // setTimeout(function(){$('#thanks').fadeOut()},3000)
-        return false;
-  }       
-});
+  $('input[name=phone]').mask("+7 (999) 999-99-99"); //маска номера
 
-$('.btn').mouseover( function(e) {
-  e.preventDefault();
-  $(this).addClass('btn__hover');
-});
-
-$('.btn').mouseout( function(e) {
-  e.preventDefault();
-  $(this).removeClass('btn__hover');
-});
+  $('form').submit(function(e) {
+    if ($('input').val() === "") {
+      return false; //если инпут пустой, то отправка не происходит 
+    } else {
+      e.preventDefault(); //отменяем перезагрузку
+      $.ajax({  //отправляем данные на сервер с помощью php mailer
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize() //собирает данные с формы и возвращает строку
+      }).done(function() {   //отправка успешна
+          $(this).find("input").val("");   
+          $(this).find("textarea").val("");
+          $('#modal').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+          $('form').trigger('reset'); //очищение формы
+      });
+      // setTimeout(function(){$('#thanks').fadeOut()},3000)
+          return false;
+    }       
+  });
 
 
-$('.btn').click(function() {
-  Swal.fire(
-    'Good job!',
-    'You clicked the button!',
-    'success'
-  );
-});
+  $('.btn').click(function() {
+    Swal.fire(
+      'Good job!',
+      'You clicked the button!',
+      'success'
+    );
+  });
 
 
- }); 
+}); 
   
 
 
